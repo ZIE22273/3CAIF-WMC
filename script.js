8ed5754f250dd39ad02f258b267d1608bb9e4488
Pascal Z. <168551715+ZIE22273@users.noreply.github.com>
2024-11-14 15:48:53 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showFontsPopup() {
-    document.getElementById('fonts-popup').style.display = 'flex';
-  }
-  
-  function closePopup() {
-    document.getElementById('fonts-popup').style.display = 'none';
-  }
-  
